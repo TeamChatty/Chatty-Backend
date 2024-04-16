cd782a6ca7d2f7a41b43c5001e2f69f93a8079b1
YunByungil <dbsquddlfz@naver.com>
2024-04-16 16:57:17 +0900

    test: 알람 수신 on/off 목록 조회 기능 테스트 코드 작성

---
+++ src/test/java/com/chatty/service/notification/NotificationReceiveServiceTest.java
@@ -0,0 +1,91 @@
+package com.chatty.service.notification;
+
+import com.chatty.constants.Authority;
+import com.chatty.dto.notification.receive.response.NotificationReceiveResponse;
+import com.chatty.entity.notification.NotificationReceive;
+import com.chatty.entity.user.Coordinate;
+import com.chatty.entity.user.Gender;
+import com.chatty.entity.user.User;
+import com.chatty.repository.notification.NotificationReceiveRepository;
+import com.chatty.repository.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NotificationReceiveServiceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private NotificationReceiveService notificationReceiveService;
+
+    @Autowired
+    private NotificationReceiveRepository notificationReceiveRepository;
+
+    @AfterEach
+    void tearDown() {
+        notificationReceiveRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("로그인한 유저의 알람 수신 목록을 조회한다. 처음 가입하면 기본 값은 true다.")
+    @Test
+    void getNotificationReceive() {
+        // given
+        User user = createUser("박지성", "01012345678");
+        userRepository.save(user);
+
+        NotificationReceive notificationReceive = createNotificationReceive(user);
+        notificationReceiveRepository.save(notificationReceive);
+
+        // when
+        NotificationReceiveResponse notificationReceiveResponse =
+                notificationReceiveService.getNotificationReceive(user.getMobileNumber());
+
+        // then
+        assertThat(notificationReceiveResponse.getNotificationReceiveId()).isNotNull();
+        assertThat(notificationReceiveResponse)
+                .extracting("userId", "marketingNotification", "chattingNotification", "feedNotification")
+                .containsExactlyInAnyOrder(user.getId(), true, true, true);
+    }
+
+    private NotificationReceive createNotificationReceive(final User user) {
+        return NotificationReceive.builder()
+                .feedNotification(true)
+                .marketingNotification(true)
+                .chattingNotification(true)
+                .user(user)
+                .build();
+    }
+
+    private User createUser(final String nickname, final String mobileNumber) {
+        return User.builder()
+                .mobileNumber(mobileNumber)
+                .deviceId("123456")
+                .authority(Authority.USER)
+//                .mbti(Mbti.ENFJ)
+                .birth(LocalDate.now())
+                .imageUrl("이미지")
+                .address("주소")
+                .gender(Gender.MALE)
+                .nickname(nickname)
+                .location(User.createPoint(
+                        Coordinate.builder()
+                                .lat(37.1)
+                                .lng(127.1)
+                                .build()
+                ))
+                .build();
+    }
+}
