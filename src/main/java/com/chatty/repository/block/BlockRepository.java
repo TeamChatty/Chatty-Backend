afb201551296e8103d51e47a6a12b136102eb7a1
YunByungil <dbsquddlfz@naver.com>
2024-05-01 15:44:55 +0900

    feat: 로그인한 유저를 기준으로 차단한 사람 Id 값 불러오는 로직 추가

--- src/main/java/com/chatty/repository/block/BlockRepository.java
+++ src/main/java/com/chatty/repository/block/BlockRepository.java
@@ -1,9 +1,18 @@
 package com.chatty.repository.block;
 
 import com.chatty.entity.block.Block;
+import com.chatty.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BlockRepository extends JpaRepository<Block, Long> {
 
     boolean existsByBlockerIdAndBlockedId(Long blockerId, Long blockedId);
+
+    @Query("select b.blocked.id " +
+            "from Block b " +
+            "where b.blocker = :blocker")
+    List<Long> findAllByBlocker(User blocker);
 }
