af2b4ab20a77b385226ddad7ee3bb5564d58afd5
YunByungil <dbsquddlfz@naver.com>
2024-05-07 14:48:45 +0900

    test: 신고 기능 Controller, Service 테스트 코드 작성

---
+++ src/test/java/com/chatty/service/report/ReportServiceTest.java
@@ -0,0 +1,126 @@
+package com.chatty.service.report;
+
+import com.chatty.constants.Authority;
+import com.chatty.dto.report.request.ReportCreateRequest;
+import com.chatty.dto.report.response.ReportResponse;
+import com.chatty.entity.report.Report;
+import com.chatty.entity.user.Gender;
+import com.chatty.entity.user.User;
+import com.chatty.exception.CustomException;
+import com.chatty.repository.report.ReportRepository;
+import com.chatty.repository.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ReportServiceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ReportService reportService;
+
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @AfterEach
+    void tearDown() {
+        reportRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("해당 유저를 신고한다.")
+    @Test
+    void createReport() {
+        // given
+        User reporter = createUser("박지성", "01012345678");
+        User reported = createUser("강혜원", "01011112222");
+        userRepository.saveAll(List.of(reporter, reported));
+
+        ReportCreateRequest request = ReportCreateRequest.builder()
+                .content("신고 사유")
+                .build();
+
+        // when
+        ReportResponse reportResponse =
+                reportService.createReport(reported.getId(), reporter.getMobileNumber(), request);
+
+        // then
+        assertThat(reportResponse.getReportId()).isNotNull();
+        assertThat(reportResponse)
+                .extracting("reporterId", "reportedId", "content")
+                .containsExactly(reporter.getId(), reported.getId(), "신고 사유");
+    }
+
+    @DisplayName("해당 유저를 신고할 때, 이미 신고한 유저면 예외가 발생한다.")
+    @Test
+    void createReportWithDuplicateReport() {
+        // given
+        User reporter = createUser("박지성", "01012345678");
+        User reported = createUser("강혜원", "01011112222");
+        userRepository.saveAll(List.of(reporter, reported));
+
+        Report report = createReport(reporter, reported, "신고 사유");
+        reportRepository.save(report);
+
+        ReportCreateRequest request = ReportCreateRequest.builder()
+                .content("신고 사유")
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> reportService.createReport(reported.getId(), reporter.getMobileNumber(), request))
+                .isInstanceOf(CustomException.class)
+                .hasMessage("이미 신고한 유저입니다.");
+    }
+
+    @DisplayName("유저를 신고할 때, 본인을 신고하면 예외가 발생한다.")
+    @Test
+    void createReportWithSelfReport() {
+        // given
+        User reporter = createUser("박지성", "01012345678");
+        userRepository.save(reporter);
+
+        ReportCreateRequest request = ReportCreateRequest.builder()
+                .content("신고 사유")
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> reportService.createReport(reporter.getId(), reporter.getMobileNumber(), request))
+                .isInstanceOf(CustomException.class)
+                .hasMessage("자기 자신을 신고할 수 없습니다.");
+    }
+
+    private Report createReport(User reporter, User reported, String content) {
+        return Report.builder()
+                .reporter(reporter)
+                .reported(reported)
+                .content(content)
+                .build();
+    }
+
+    private User createUser(final String nickname, final String mobileNumber) {
+        return User.builder()
+                .mobileNumber(mobileNumber)
+                .deviceId("123456")
+                .authority(Authority.USER)
+                .birth(LocalDate.now())
+                .imageUrl("이미지")
+                .address("주소")
+                .gender(Gender.MALE)
+                .nickname(nickname)
+                .candy(10)
+                .ticket(10)
+                .build();
+    }
+}
